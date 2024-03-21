1a83cb00c4732c56faadbb343dfcfcf3f781b031
Miguel Perez <90426853+TGMAPA@users.noreply.github.com>
2024-03-21 17:42:34 -0600

    Update sensor.py
    -Corregí métodos get().
    -Agregue e implemente métodos is() para todos los sensores.
    -Indiqué las anotaciones de tipo de dato de salida.
    -Indiqué tipo de datos para los parámetros de todos los métodos.

--- Robot/sensor.py
+++ Robot/sensor.py
@@ -7,8 +7,11 @@ class sens:
     def __init__():
         pass
 
-    def conectar(self,puerto):
-        self.ard=serial.Serial(puerto,9600)
+    def conectar(self,puerto:str):
+        try:
+            self.ard=serial.Serial(puerto,9600)
+        except:
+            return False
     
     def leer(self):
         time.sleep(1)
@@ -19,24 +22,38 @@ class sens:
             print(dp)
     
     def desColor(self):
-        self.ard.write(b'1')
+        try:
+            self.ard.write(b'1')
+            return True
+        except:
+            return False
 
-    #def desTouch(self):
-     #   self.ard.write(b'3')
-    
+    def desTouch(self):
+        try:
+            self.ard.write(b'3')
+        except:
+            return False
+
     def desDistance(self):
-        self.ard.write(b'4')
+        try:
+            self.ard.write(b'4')
+        except:
+            return False
 
     def desPresence(self):
-        self.ard.write(b'5')
+        try:
+            self.ard.write(b'5')
+        except:
+            return False
 
-    def getColor(int op=0):
-        dt=self.ard.readline()
-        dt=dt.dplit('@')
-        dt=dt.split(',')
-        dt[0]=int(dt[0])
-        dt[1]=int(dt[1])
-        dt[2]=int(dt[2])
+    def getColor(self, op:int=0) ->bool | list:
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=rawd[0].split(',') # Split into RGB List
+        dt[0]=int(dt[0]) #R
+        dt[1]=int(dt[1]) #G
+        dt[2]=int(dt[2]) #B
         # Si op==0, regresa si es rojo, verde o azul
         if(op==0):
             if(dt[0]>dt[1] and dt[0]>dt[2]):
@@ -47,74 +64,123 @@ class sens:
                 return "blue"
         #Si op!=0, regresa el código rgb
         else:
-            return dt
+            return dt # Return RGB List 
     
-    def isColor(self,col:string):
-        return getColor()==col
+    def isColor(self, col:str) ->bool :
+        return self.getColor()==col
 
-    def isColor(self,col:list):
-        dt=getColor(1)
+    def isColor(self,col:list)  ->bool :
+        dt=self.getColor(1)
         return dt==col
 
-    def getSound(self):
-        dt=self.ard.readline()
-        dt=dt.split('@')
-        dt=dt[1]
+    def getSound(self)  ->bool :
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=rawd[1] # Sonido registrado
         if(dt=='1'):
             return True
         else:
             return False
+        
+    def isSound(self) ->bool :
+        return self.getSound()
 
-    def getTouch(self):
-        dt=self.ard.readline()
-        dt=dt.split('@')
-        dt=dt[2]
+    def getTouch(self)  ->bool :
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=rawd[2]
         if(dt==1 or dt==2):
             return True
         else:
             return False
+        
+    def isTouch(self)  ->bool :
+        return self.getTouch()
 
-    def getShortTouch(self):
-        dt=self.ard.readline()
-        dt=dt.split('@')
-        dt=dt[2]
-        if(dt=='1'):
+    def getShortTouch(self)  ->bool :
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=rawd[2]
+        if(dt=='1'): 
             return True
         else:
             return False
-    def getLongTouch(self):
-        dt=self.ard.readline()
-        dt=dt.split('@')
-        dt=dt[2]
+        
+    def isShortTouch(self)  ->bool :
+        return self.getShortTouch()
+
+    def getLongTouch(self)  ->bool :
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=rawd[2]
         if(dt=='2'):
             return True
         else:
             return False
+        
+    def isLongTouch(self)  ->bool :
+        return self.getLongTouch()
 
-    def getDistance(self):
-        dt=self.ard.readline()
-        dt=dt.split('@')
-        dt=float(dt[3])
+    def getDistance(self)  ->float :
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=float(rawd[3])
         return dt
+    
+    def isDistance(self, infLimit:int|float, supLimit:int|float)  ->bool :
+        if(self.getDistance() >= infLimit and self.getDistance() <= supLimit):
+            return True
+        else:
+            return False
 
-    def getPresence(self):
-        dt=self.ard.readline()
-        dt=dt.split('@')
-        dt=dt[4]
+    def getPresence(self)  ->list[int] :
+        rawd=self.ard.readline() # Reading Raw data 
+        rawd=str(rawd,"utf-8")
+        rawd=rawd.split('@') 
+        dt=rawd[4]
         dt=dt.split(",")
+        dt[1]= int(dt[1]) # Sensor 1
+        dt[2]= int(dt[2]) # Sensor 2
+        dt[3]= int(dt[3]) # Sensor 3
         return dt
+    
+    def getPresence(self, sensorid:int)  ->int | bool :
+        if(sensorid<1 or sensorid>3):
+            return False
+        else:
+            rawd=self.ard.readline() # Reading Raw data 
+            rawd=str(rawd,"utf-8")
+            rawd=rawd.split('@') 
+            dt=rawd[4]
+            dt=dt.split(",")
+            dt = int(dt[sensorid])
+            return dt
+    
+    def isPresence(self, sensorid:int)  ->bool :
+        if(sensorid<1 or sensorid>3):
+            return False
+        else:
+            if(self.getPresence(sensorid)!=0):
+                return True
+            else:
+                return False
 
     #Método until incompleto (no dejar avanzar hasta que se cumpla una condición)
-    def until(self,sen,cond):
+    def until(self, sen:str, cond:any)  ->bool :
         while True:
-            if(sen=="color" and isColor(cond)):
+            if(sen=="color" and self.isColor(cond)):
                return True
-            elif(sen=="sonido" and getSound()=='1'):
+            elif(sen=="sonido" and self.getSound()=='1'):
                 return True
             elif(sen=="toque"):
-                if(cond=='0' and getTouch()):
+                if(cond=='0' and self.getTouch()):
                     return True
-                elif(cond=='1' and getShortTouch()):
+                elif(cond=='1' and self.getShortTouch()):
                     return True
-                elif(getLongTouch()):
+                elif(self.getLongTouch()):
                     return True
